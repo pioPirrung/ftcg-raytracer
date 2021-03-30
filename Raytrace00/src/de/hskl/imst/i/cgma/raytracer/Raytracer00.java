48c920e6e4ba715dabcf9fea3ddc01e5d43706ce
pioPirrung <pirrung@pioneo.de>
2021-03-30 10:01:27 +0200

    Test Workspace

--- Raytrace00/src/de/hskl/imst/i/cgma/raytracer/Raytracer00.java
+++ Raytrace00/src/de/hskl/imst/i/cgma/raytracer/Raytracer00.java
@@ -54,6 +54,7 @@ public class Raytracer00 implements IRayTracerImplementation {
 	    // set attributes h, w
 	    h = (float) (2 * near * Math.tan(fovyDegree/2));
 	    w = h * aspect;
+	    //Workspace test
     }
 
     @Override
